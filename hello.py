ffcf05dfb82cc7974e0f887c3ed61993d1dfc1fe
jedidiahT <its.theo.no.cameo@gmail.com>
2026-04-19 09:45:55 +0200

    Module 1: First Python program and solar problem statements

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello, World! My name is Jedidiah and I am building a solar AI for Zimbabwe.")
+print("Problem 1: Installers have a tough time getting personalized fault-finding assistance as most troubleshooting methods are not tailored to our region in particular")
+print("Problem 2: Solar technology and innovation is advancing rapidly and legacy installers are getting left behind")
+print("Problem 3: System variables make fault-finding a bigger challenge than it should be")
