24fd0229b2061922c8246da2844bfba20c05b318
jedidiahT <its.theo.no.cameo@gmail.com>
2026-04-20 08:20:42 +0200

    Module 1: Variables lesson and client setup

---
+++ clientsetup.py
@@ -0,0 +1,14 @@
+client_name = "MR. MOYO"
+system_size = "48V HOME BACKUP"
+main_components = "11kVA DEYE Inverter, 10.24kWH LiFePO4 Battery Pack, 12* 700watts mono solar panels"
+installation_date = "24th of June, 2026"
+city = "Mutare"
+country = "Zimbabwe"
+technician = "Engineer Jedidiah"
+
+print(f"Client name: {client_name}")
+print(f"Recommended setup: {system_size}")
+print(f"What's Needed: {main_components}")
+print(f"When: {installation_date}")
+print(f"Where: {city}, {country}")
+print(f"Project Head: {technician}")
---
+++ variables.py
@@ -0,0 +1,10 @@
+technician_name = "Jedidiah"
+city = "Mutare"
+country = "Zimbabwe"
+solar_experience_years = "5"
+project_objective = "Make solar easy to understand"
+
+print(f"Technician name: {technician_name}")
+print(f"Location: {city}, {country}")
+print(f"Years of experience: {solar_experience_years}")
+print(f"Project Goal: {project_objective}")
