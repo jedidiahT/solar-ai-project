c6d82303cd11fd18552a607f63ab9b18f151022e
jedidiahT <its.theo.no.cameo@gmail.com>
2026-09-13 12:11:20 +0200

    Module 2: while loop for multiple client sessions

--- functions.py
+++ functions.py
@@ -17,11 +17,17 @@ print(f"Total system wattage: {result}watts")
 
 def solar_report(client_name, panels, wattage, battery_voltage):
     total_wattage = panels * wattage
-    system_ok = battery_voltage > 47
+    system_ok = battery_voltage > 47.5
     print(f"Client: {client_name}")
     print(f"Total System Wattage: {total_wattage }W")
     print(f"Battery Voltage: {battery_voltage}V")
-    print(f"Solar Status OK: {system_ok}")
+    if battery_voltage > 50:
+        print("System Status: Medium/Heavy Loads Acceptable.")
+    elif battery_voltage > 47.5:
+        print("System Status: Consider lowering Battery load for longer backup.")
+    else:
+        print("Warning: Battery voltage low. Lower load count to only essentials") 
+       
 
 client_name = input("Enter client name: ")
 panels = int(input("Enter number of panels: "))
@@ -30,3 +36,11 @@ battery_voltage = float(input("Enter battery voltage: "))
 
 solar_report(client_name, panels, wattage, battery_voltage)
 
+while True:
+    client_name = input("Enter client name (or 'quit' to exit): ")
+    if client_name == "quit":
+        break
+    panels = int(input("Enter number of panels: "))
+    wattage = int(input("Enter panel wattage: "))
+    battery_voltage = float(input("Enter battery voltage: "))
+    solar_report(client_name, panels, wattage, battery_voltage)
